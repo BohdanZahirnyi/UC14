d1dd20a5c492adf971c9ea031e65882dd40afdf0
Bohdan Zahirnyi <bzahi@softserveinc.com>
2023-07-11 16:37:48 +0300

    UC#14 Infrastructure setup

---
+++ UC14Localization/UC14Localization/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UC14Localization.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
